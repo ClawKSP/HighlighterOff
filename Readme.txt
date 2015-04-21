a86d536f226eaefa7541068500a29c1e29f00648
ClawKSP <ClawKSP@gmail.com>
2015-04-20 21:22:20 -1000

    HighlighterOff
    Disables part highlighting while in flight mode.

---
+++ Readme.txt
@@ -0,0 +1,27 @@
+HighlighterOff
+=========================
+- Disables the mouse-over highlighting for vessels in flight mode.
+
+
+Installation
+============
+- Just place the HighlighterOff folder in your GameData directory.
+
+
+UnInstall
+=========
+- To uninstall, just delete the HighlighterOff folder.
+
+
+
+License
+=======
+Covered under the CC-BY-NC-SA license. See the license.txt for more details.
+
+(https://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+
+Change Log
+
+==========
+v0.01.00 (20 Apr 15) - Initial release
---
+++ Source/HighlighterOff.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * This module written by Claw. For more details, please visit
+ * http://forum.kerbalspaceprogram.com/threads/97285
+ * 
+ * This mod is covered under the CC-BY-NC-SA license. See the license.txt for more details.
+ * (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+ * 
+ *
+ * HighlighterOff - Written for KSP v0.90.0
+ * 
+ * - Disables highlighting for parts in flight mode.
+ * 
+ * Change Log:
+ * - v01.00    Initial Release
+ * 
+ */
+
+using UnityEngine;
+using KSP;
+
+
+namespace ClawKSP
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class HighlighterOff : MonoBehaviour
+    {
+        public void Start ()
+        {
+            Debug.LogWarning("HighlighterOff.Start(): version 1.0");
+            GameEvents.onVesselGoOffRails.Add(OffRails);
+        }
+
+        public void OffRails (Vessel vesselToFix)
+        {
+            Debug.Log("HighlighterOff.OffRails(): Disabling Highlight");
+
+            for (int indexParts = 0; indexParts < vesselToFix.Parts.Count; indexParts++)
+            {
+                vesselToFix.Parts[indexParts].highlightType = Part.HighlightType.Disabled;
+            }
+        }
+
+        public void OnDestroy ()
+        {
+            Debug.Log("HighlighterOff.OnDestroy()");
+            GameEvents.onVesselGoOffRails.Remove(OffRails);
+        }
+    }
+}
